e47d68b17aae30575046bf1bdedcd4e24b0c0ce1
Italo Gabriel <Italog03@gmail.com>
2023-03-01 20:47:07 -0300

    feat: download video to videoPlayer

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -5,11 +5,13 @@ async function main() {
   const isLocal = !!~localHost.indexOf(window.location.hostname);
   console.log("isLocal", isLocal);
   const manifestJSON = await (await fetch(MANIFEST_URL)).json();
-  const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost;
+  const host = isLocal ? manifestJSON.localhost : manifestJSON.productionHost;
   const videoComponent = new VideoComponent();
+  const network = new Network({ host });
 
   const videoPlayer = new VideoMediaPlayer({
     manifestJSON,
+    network,
   });
 
   videoPlayer.initializeCodec();
